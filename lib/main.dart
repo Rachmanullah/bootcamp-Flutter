fd6e732e01b3980f781a9f1b826973177a139789
Rachmanullah <rachmanullah1@gmail.com>
2023-09-07 19:35:08 +0700

    Firebase (masih error)

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning/presentation/router/pages.dart';
+import 'package:learning/presentation/router/routes.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      getPages: Pages.getAllPages,
+      initialRoute: Routes.splashScreen,
+    );
+  }
+}
---
+++ lib/presentation/screen/home/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning/presentation/screen/home/home_controller.dart';
+import 'package:learning/presentation/screen/home/widgets/banner_builder.dart';
+import 'package:learning/presentation/screen/home/widgets/welcoming_widget.dart';
+import 'package:learning/presentation/screen/widgets/section_title.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  final homeController = Get.find<HomeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F7F8),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Rachman',
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Selamat datang',
+              style: TextStyle(fontSize: 10),
+            ),
+          ],
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: CircleAvatar(
+              backgroundColor: Colors.red,
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const WelcomingWidget(),
+              const SizedBox(height: 16),
+              const SectionTitle(title: 'Terbaru'),
+              const SizedBox(height: 8),
+              GetBuilder<HomeController>(
+                  init: homeController,
+                  initState: (state) => homeController.getBanners(),
+                  builder: (hCon) {
+                    final bannerList = hCon.banner;
+
+                    return bannerList.isEmpty
+                        ? const Center(child: CircularProgressIndicator())
+                        : BannerBuilder(bannerList: bannerList);
+                  }),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SectionTitle(title: 'Pilih Pelajaran'),
+                  TextButton(onPressed: () {}, child: const Text('Lihat Semua'))
+                ],
+              ),
+              const SizedBox(height: 8),
+              GetBuilder<HomeController>(
+                  init: homeController,
+                  initState: (state) => homeController.getCourse('IPA'),
+                  builder: (cHome) {
+                    final courseList = cHome.courseList;
+
+                    return courseList.isEmpty
+                        ? const Center(child: CircularProgressIndicator())
+                        : const Center(child: CircularProgressIndicator());
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
